bb01596b76103dd5fef755dd056a5a9ab93a97ce
Wai Hwa Tan <waihwata@usc.edu>
2015-08-03 19:39:53 -0700

    added record column

--- client/controllers/StoryController.js
+++ client/controllers/StoryController.js
@@ -5,7 +5,7 @@ StoryController.$inject = ['$scope','$http'];
 function StoryController($scope, $http){
 	this.storyText = '';
 	this.submit = function(){
-		this.storyText += ' '+this.userInput;
+		this.storyText += ' ' + this.userInput;
 		this.userInput = '';
 	}
 }
